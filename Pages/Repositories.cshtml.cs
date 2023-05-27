8c2d2995e048e651d5784d16846e6bea981f8ef9
Rusty <rusty@rddsoftware.com>
2023-05-26 23:52:23 -0700

    Test

---
+++ Pages/Repositories.cshtml.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using RestSharp;
+
+namespace rddSoftwareSite.Pages
+{
+    public class RepositoriesModel : PageModel
+    {
+        //  Get the description from a GitHub repository
+
+
+        public void OnGet()
+        {
+        }
+    }
+}
